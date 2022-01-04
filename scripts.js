76517db81c263262f12d8fa4dd3f6d79c04a8d91
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-04 09:51:49 +0000

    Added some JavaScript

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//Print to console
+console.log("Hello!");
+console.log(4 + 4);
+
+//Variables
+var total = 14.0;
+console.log("£" + total); //Concatentation
+console.log(`The total price is £${total}.`)
