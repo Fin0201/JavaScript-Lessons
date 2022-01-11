bb7807b220b855e19d10e1b899b71a643c21b6aa
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-11 09:59:07 +0000

    Added more button JavaScript

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 //Invoice example
+/*
 var sign = "Bristol Zoo!";
 var length = sign.length;
 var subtotal = length * 2.99;
@@ -9,4 +10,21 @@ document.getElementById("userSign").innerHTML= sign;
 document.getElementById("tiles").textContent = length;
 document.getElementById("subtotal").textContent = `£${subtotal}`;
 document.getElementById("shipping").textContent = `£${shipping}`;
-document.getElementById("total").textContent = `£${total}`;
+document.getElementById("total").textContent = `£${total.toFixed(2)}`;
+*/
+function CalculateCost() {
+    var sign = document.getElementById("user").value;
+    document.getElementById("userSign").innerHTML = sign;
+
+    var sign = "Bristol Zoo!";
+var length = sign.length;
+var subtotal = length * 2.99;
+var shipping = 4.99;
+var total = subtotal+shipping;
+
+document.getElementById("userSign").innerHTML= sign;
+document.getElementById("tiles").textContent = length;
+document.getElementById("subtotal").textContent = `£${subtotal}`;
+document.getElementById("shipping").textContent = `£${shipping}`;
+document.getElementById("total").textContent = `£${total.toFixed(2)}`;
+  }
